598a2bb3fc60b07e85809d73ccbffd5108159ac6
Tafadzwa Gonera <tafadzwagonera@gmail.com>
2024-08-03 14:16:57 +0200

    chore(configuration): logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import 'dotenv/config'
 import { AppModule } from './app.module'
+import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import env from 'config/env.config'
+import NodeEnv from 'common/enums/node-env.enum'
+import type { LogLevel } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const isProd = env.get('NODE_ENV') !== NodeEnv.Dev
+
+  const logLevel: LogLevel[] = isProd
+    ? ['error', 'fatal', 'log']
+    : ['debug', 'error', 'fatal', 'log', 'verbose', 'warn'];
+
+  const app = await NestFactory.create(AppModule, {
+    logger: logLevel,
+  })
 
   await app.listen(3000)
+
+  Logger.log(`Server listening at ${await app.getUrl()}`)
 }
 
 bootstrap()
