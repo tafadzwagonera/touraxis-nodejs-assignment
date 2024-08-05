2cdb6907afede0264606344b64a43b1d1f1cf1a6
Tafadzwa Gonera <tafadzwagonera@gmail.com>
2024-08-05 08:37:09 +0200

    style(prettier): enforce consistency

--- src/entities/task.entity.ts
+++ src/entities/task.entity.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { CustomBase } from './custom-base.entity'
-import { DateType, Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core'
+import {
+  DateType,
+  Entity,
+  ManyToOne,
+  PrimaryKey,
+  Property,
+} from '@mikro-orm/core'
 import { ObjectId } from '@mikro-orm/mongodb'
 import { User } from './user.entity'
 
